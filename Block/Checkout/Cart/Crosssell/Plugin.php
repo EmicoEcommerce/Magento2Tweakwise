a5a401e27f199108573d1ce365e4e47a5f4d0840
ah-net <103565001+ah-net@users.noreply.github.com>
2023-01-31 11:54:53 +0100

    Update Plugin.php

--- Block/Checkout/Cart/Crosssell/Plugin.php
+++ Block/Checkout/Cart/Crosssell/Plugin.php
@@ -19,6 +19,7 @@ use Magento\Framework\Registry;
 use Tweakwise\Magento2Tweakwise\Block\Catalog\Product\ProductList\AbstractRecommendationPlugin;
 use Tweakwise\Magento2Tweakwise\Exception\ApiException;
 use Tweakwise\Magento2Tweakwise\Model\Catalog\Product\Recommendation\Context;
+use Tweakwise\Magento2Tweakwise\Model\Client\Request\Recommendations\FeaturedRequest;
 use Tweakwise\Magento2Tweakwise\Model\Client\Request\Recommendations\ProductRequest;
 use Tweakwise\Magento2Tweakwise\Model\Client\RequestFactory;
 use Tweakwise\Magento2Tweakwise\Model\Config;
@@ -75,7 +76,13 @@ class Plugin extends AbstractRecommendationPlugin
      */
     protected function getType()
     {
-        return Config::RECCOMENDATION_TYPE_SHOPPINGCART;
+        $crosssellType = $this->config->getShoppingcartCrossellType();
+
+        if (empty($crosssellType) || $crosssellType === 'crosssell') {
+            return Config::RECCOMENDATION_TYPE_SHOPPINGCART;
+        }
+
+        return Config::RECCOMENDATION_TYPE_SHOPPINGCART_FEATURED;
     }
 
     public function aroundGetLastAddedProduct(Crosssell $crossell, Closure $proceed)
@@ -169,6 +176,12 @@ class Plugin extends AbstractRecommendationPlugin
     private function getShoppingcartCrosssellTweakwiseItems (ProductInterface $product, array $result, array $cartItems) {
         $items = [];
 
+        //show featured products
+        if ($this->getType() === Config::RECCOMENDATION_TYPE_SHOPPINGCART_FEATURED) {
+            return $this->getFeaturedItems();
+        }
+
+        //show crosssell products
         $requestFactory = new RequestFactory(ObjectManager::getInstance(), ProductRequest::class);
         $request = $requestFactory->create();
         $request->setProduct($product);
@@ -238,5 +251,32 @@ class Plugin extends AbstractRecommendationPlugin
         }
 
         return $products;
+    }
+
+    private function getFeaturedItems()
+    {
+        $requestFactory = new RequestFactory(ObjectManager::getInstance(), FeaturedRequest::class);
+        $request = $requestFactory->create();
+
+        $templateId = $this->config->getRecommendationsTemplate(Config::RECCOMENDATION_TYPE_SHOPPINGCART_FEATURED);
+        $request->setTemplate($templateId);
+
+        $this->recommendationsContext->setRequest($request);
+
+        try {
+            $collection = $this->recommendationsContext->getCollection();
+        } catch (ApiException $e) {
+            return [];
+        }
+
+        if (!empty($cartItems)) {
+            $collection = $this->removeCartItems($collection, $cartItems);
+        }
+
+        foreach ($collection as $item) {
+            $items[] = $item;
+        }
+
+        return $items;
     }
 }

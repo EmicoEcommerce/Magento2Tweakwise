e977fa7b927c36c682c8959714a7233e8430b769
ah-net <103565001+ah-net@users.noreply.github.com>
2023-11-14 12:58:12 +0100

    Fixes ALP facets for multiple stores

--- Controller/Ajax/FacetAttributes.php
+++ Controller/Ajax/FacetAttributes.php
@@ -46,21 +46,33 @@ class FacetAttributes extends Action
 
         $categoryId = $this->getRequest()->getParam('category');
         $facetKey = $this->getRequest()->getParam('facetkey');
-        //remove category id for now. It can give the wrong store id for the admin which results in the wrong tncid
-        //$facetRequest->addCategoryFilter($categoryId);
+        $allStores = $facetRequest->getStores();
         $facetRequest->addFacetKey($facetKey);
 
         $result = [];
-        try {
-            $response = $this->client->request($facetRequest);
-            foreach ($response->getAttributes() as $attribute) {
-                $result[] = ['value' => $attribute['title'], 'label' => $attribute['title']];
+        foreach ($allStores as $store) {
+            $facetRequest->setStore($store->getId());
+            if (!empty($categoryId)) {
+                $facetRequest->addCategoryFilter($categoryId);
             }
-        } catch (ApiException $e) {
-            if (!$e->getCode() == 404) {
-                throw $e;
+            try {
+                $response = $this->client->request($facetRequest);
+            } catch (ApiException $e) {
+                if (!$e->getCode() == 404) {
+                    throw $e;
+                }
+                continue;
+            }
+
+            if (!empty($response->getAttributes())) {
+                foreach ($response->getAttributes() as $attribute) {
+                    $result[] = ['value' => $attribute['title'], 'label' => $attribute['title']];
+                }
             }
         }
+
+        $result = array_unique($result, SORT_REGULAR);
+
 
         $json->setData(['data' => $result]);
         return $json;

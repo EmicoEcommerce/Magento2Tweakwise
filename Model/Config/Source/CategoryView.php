906f6380c65c51dec1ee714923f1590fbd10ec62
ah-net <103565001+ah-net@users.noreply.github.com>
2023-12-08 15:01:09 +0100

    Rename the new setting

---
+++ Model/Config/Source/CategoryView.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Tweakwise (https://www.tweakwise.com/) - All Rights Reserved
+ *
+ * @copyright Copyright (c) 2017-2022 Tweakwise.com B.V. (https://www.tweakwise.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Tweakwise\Magento2Tweakwise\Model\Config\Source;
+
+use Tweakwise\Magento2Tweakwise\Model\Catalog\Layer\Url\Strategy\PathSlugStrategy;
+use Tweakwise\Magento2Tweakwise\Model\Catalog\Layer\Url\Strategy\QueryParameterStrategy;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class CategoryView implements OptionSourceInterface
+{
+    /**
+     * Possible filter types
+     */
+    public const EXTENDED = 1;
+    public const SIMPLE = 0;
+
+    /**
+     * @var array[]
+     */
+    protected $options;
+
+    /**
+     * @return array
+     */
+    protected function buildOptions()
+    {
+        return [
+            ['value' => self::EXTENDED, 'label' => __('Extended')],
+            ['value' => self::SIMPLE, 'label' => __('Simple (deprecated)')],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        if (!$this->options) {
+            $this->options = $this->buildOptions();
+        }
+        return $this->options;
+    }
+}

b058219faf671639a4a5b2c0c0789907d05eccf8
ah-net <103565001+ah-net@users.noreply.github.com>
2024-05-21 15:32:29 +0200

    fix: remove duplicate facet keys

--- Controller/Ajax/FacetAttributes.php
+++ Controller/Ajax/FacetAttributes.php
@@ -86,7 +86,12 @@ class FacetAttributes extends Action
 
         $result[] = ['value' => 'tw_other', 'label' => 'Other (text field)'];
 
-        $result = array_unique($result, SORT_REGULAR);
+        //prevent duplicate keys
+        $values = array_column($result, 'value');
+        $uniqueValues = array_unique($values);
+        $uniqueKeys = array_keys($uniqueValues);
+        $result = array_intersect_key($result, array_flip($uniqueKeys));
+
 
         //prevent non sequential array keys. That causes json encode to act differently and creates objects instead of arrays
         $result = array_values($result);

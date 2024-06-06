108f14e9543aa9956813ec475a06c9a9056e6c9a
ah-net <103565001+ah-net@users.noreply.github.com>
2024-06-06 13:35:22 +0200

    feat: cancel prev autocomplete requests (#187)
    feat: cancel prev autocomplete requests

--- view/frontend/web/js/quick-search.js
+++ view/frontend/web/js/quick-search.js
@@ -11,6 +11,9 @@ define([
 ], function ($, quickSearch) {
     $.widget('tweakwise.quickSearch', quickSearch, {
 
+        // Declare the variable to hold the current AJAX request
+        currentRequest: null,
+
         getSelectedProductUrl: function () {
             if (!this.responseList.selected) {
                 return null;
@@ -23,6 +26,21 @@ define([
             $(this.options.formSelector).on('submit', function (event) {
                 if (this.getSelectedProductUrl()) {
                     event.preventDefault();
+                }
+            }.bind(this));
+
+            // Use jQuery's ajaxSend event to abort the previous AJAX request before a new one is sent
+            $(document).ajaxSend(function (event, jqXHR, ajaxOptions) {
+
+                // Get the request URL and type from ajaxOptions
+                var requestUrl = ajaxOptions.url;
+                // If there's an ongoing AJAX request, abort it if it is an search request and if the new request is an search request
+                if (this.currentRequest && requestUrl.indexOf('search/ajax') !== -1) {
+                    this.currentRequest.abort();
+                    // Store the jqXHR object of the new AJAX request
+                    this.currentRequest = jqXHR;
+                } else if (requestUrl.indexOf('search/ajax') !== -1) {
+                    this.currentRequest = jqXHR;
                 }
             }.bind(this));
 

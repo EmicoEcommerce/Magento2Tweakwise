2780fa52c42d2089ef8f383b0b716ff366bb7d0e
ah-net <103565001+ah-net@users.noreply.github.com>
2025-11-06 13:18:07 +0100

    Change upgradeSchema to patch

---
+++ Setup/Patch/Schema/ChangePrimaryKeyTweakwiseAttributeSlugTable.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tweakwise\Magento2Tweakwise\Setup\Patch\Schema;
+
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class ChangePrimaryKeyTweakwiseAttributeSlugTable implements SchemaPatchInterface
+{
+    private $schemaSetup;
+
+    public function __construct(SchemaSetupInterface $schemaSetup)
+    {
+        $this->schemaSetup = $schemaSetup;
+    }
+
+    public function apply()
+    {
+        $setup = $this->schemaSetup;
+        $setup->startSetup();
+        $connection = $setup->getConnection();
+        $tableName = $setup->getTable('tweakwise_attribute_slug');
+
+        $connection->dropIndex($tableName, 'PRIMARY');
+
+        $connection->addColumn($tableName, 'id', [
+            'type' => Table::TYPE_INTEGER,
+            'nullable' => false,
+            'identity' => true,
+            'unsigned' => true,
+            'comment' => 'ID',
+            'primary' => true,
+        ]);
+
+        $connection->addColumn($tableName, 'store_id', [
+            'type' => Table::TYPE_INTEGER,
+            'nullable' => false,
+            'unsigned' => true,
+            'comment' => 'store id',
+            'default' => 0,
+        ]);
+
+        $connection->addIndex($tableName, 'ATTRIBUTE', ['attribute', 'store_id'], 'unique');
+
+        $setup->endSetup();
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}

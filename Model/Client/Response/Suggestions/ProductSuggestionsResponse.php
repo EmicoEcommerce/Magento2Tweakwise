5d378a1d86a9af1013c3fc77c5f0dc26d5bfc11d
ahuininga-orisha <>
2025-12-19 11:35:59 +0100

    wip

--- Model/Client/Response/Suggestions/ProductSuggestionsResponse.php
+++ Model/Client/Response/Suggestions/ProductSuggestionsResponse.php
@@ -4,6 +4,7 @@ namespace Tweakwise\Magento2Tweakwise\Model\Client\Response\Suggestions;
 
 use Tweakwise\Magento2Tweakwise\Model\Client\Response;
 use Tweakwise\Magento2Tweakwise\Model\Client\Response\AutocompleteProductResponseInterface;
+use function array_merge;
 
 class ProductSuggestionsResponse extends Response implements AutocompleteProductResponseInterface
 {
@@ -12,13 +13,19 @@ class ProductSuggestionsResponse extends Response implements AutocompleteProduct
      */
     public function getProductIds()
     {
-        $ids = [];
-        // @phpstan-ignore-next-line
-        foreach ($this->getItems() as $item) {
-            $ids[] = $this->helper->getStoreId($item->getId());
+        $items = $this->getItems() ?? [];
+        foreach ($this->getBlocks() ?? [] as $block) {
+            foreach ($block as $bucket) {
+                $items = array_merge($items, $bucket['items'] ?: []);
+            }
         }
 
-        return $ids;
+        if (empty($items)) {
+            return [];
+        }
+
+        return array_map(fn($item) => $this->helper->getStoreId($item->getId()), $items);
+
     }
 
     /**

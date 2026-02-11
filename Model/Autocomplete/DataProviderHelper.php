eb1872093b0e04354f834d2c4cf2f1c1b4228bf5
ahuininga-orisha <>
2026-02-11 16:06:13 +0100

    wip

--- Model/Autocomplete/DataProviderHelper.php
+++ Model/Autocomplete/DataProviderHelper.php
@@ -177,6 +177,7 @@ class DataProviderHelper
 
             $product->setData('tweakwise_price', $item['tweakwise_price']);
             $product->setData('tweakwise_final_price', $item['tweakwise_final_price']);
+            $product->setData('tweakwise_id', $item['tweakwise_id']);
 
             $result[] = $this->productItemFactory->create(['product' => $product]);
         }

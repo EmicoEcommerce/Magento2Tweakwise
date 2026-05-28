67959ad6177850c274085a2866d25593c92e28ac
ahuininga-orisha <>
2026-05-28 09:19:56 +0200

    feat: expose visual item attributes to template via VisualInterface

--- Api/Data/VisualInterface.php
+++ Api/Data/VisualInterface.php
@@ -52,4 +52,17 @@ interface VisualInterface
      * @return self
      */
     public function setRowspan(int $rowspan): self;
+
+    /**
+     * Returns all Tweakwise item attributes as a flat key => value(s) array.
+     *
+     * @return array<string, string|string[]>
+     */
+    public function getVisualAttributes(): array;
+
+    /**
+     * @param array<string, string|string[]> $attributes
+     * @return self
+     */
+    public function setVisualAttributes(array $attributes): self;
 }
--- Model/Visual.php
+++ Model/Visual.php
@@ -74,4 +74,21 @@ class Visual extends Product implements VisualInterface
     {
         return $this->setData(self::ROWSPAN, $rowspan);
     }
+
+    /**
+     * @return array<string, string|string[]>
+     */
+    public function getVisualAttributes(): array
+    {
+        return $this->getData('visual_attributes') ?? [];
+    }
+
+    /**
+     * @param array<string, string|string[]> $attributes
+     * @return VisualInterface
+     */
+    public function setVisualAttributes(array $attributes): VisualInterface
+    {
+        return $this->setData('visual_attributes', $attributes);
+    }
 }

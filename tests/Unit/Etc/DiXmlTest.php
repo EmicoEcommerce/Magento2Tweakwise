53d760c787734830fd23ca298183a63eceee5f52
ahuininga-orisha <ahuininga-orisha@users.noreply.github.com>
2026-07-23 09:47:35 +0200

    Add tests

---
+++ tests/Unit/Etc/DiXmlTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tweakwise\Test\Unit\Etc;
+
+use Emico\CodeCept\Test\Unit;
+use Tweakwise\Test\Support\UnitTester;
+
+class DiXmlTest extends Unit
+{
+    protected UnitTester $tester;
+
+    public function testUpsellAndRelatedRecommendationContextsAreNotShared(): void
+    {
+        $diXmlPath = dirname(__DIR__, 3) . '/src/etc/di.xml';
+        $document = simplexml_load_file($diXmlPath);
+
+        $this->assertNotFalse($document);
+
+        $upsellContexts = $document->xpath('/config/virtualType[@name="Tweakwise\\Magento2Tweakwise\\Model\\Catalog\\Product\\Recommendation\\Context\\Product\\Upsell"]');
+        $relatedContexts = $document->xpath('/config/virtualType[@name="Tweakwise\\Magento2Tweakwise\\Model\\Catalog\\Product\\Recommendation\\Context\\Product\\Related"]');
+
+        $this->assertCount(1, $upsellContexts);
+        $this->assertCount(1, $relatedContexts);
+        $this->assertSame('false', (string) $upsellContexts[0]->attributes()->shared);
+        $this->assertSame('false', (string) $relatedContexts[0]->attributes()->shared);
+    }
+}
